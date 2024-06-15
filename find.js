e9c41f97633b5f8fb92fc8e363a32b746cbe3e6b
sowmik-sec <sowmik.sec@gmail.com>
2024-06-15 10:17:45 +0600

    Foreach, Filter, Find, And Differences Between Them

---
+++ find.js
@@ -0,0 +1,4 @@
+const playersHight = [75, 65, 67, 72, 55, 59];
+
+const selected = playersHight.find((player) => player > 70);
+console.log(selected);
---
+++ forEach.js
@@ -0,0 +1,7 @@
+const numbers = [4, 5, 2, 8, 10];
+//  numbers.forEach((num) => console.log(num));
+
+const playersHight = [75, 65, 67, 72, 55, 59];
+
+const selected = playersHight.filter((p) => p > 70);
+// console.log(selected);
